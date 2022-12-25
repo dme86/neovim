82854d9e070bc14370d49f0f1d40a32e1bf40904
Dan <me.daniel.meier@gmail.com>
2022-12-25 10:22:10 +0100

    rewriting config to lua, adding toggleterm

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- global settings
+vim.g.mapleader = ","
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.updatetime = 100
+vim.opt.cursorline = true
+vim.cmd('colorscheme gruvbox')
+
+
+-- Find files using Telescope command-line sugar.
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true})
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true})
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true})
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true})
+
+-- barbar
+-- switch between tabs via Shift+Alt & Arrow
+vim.keymap.set("n", "<S-A-Right>", '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<S-A-Left>", '<Cmd>BufferNext<CR>', { noremap = true, silent = true })
+
+-- tree settings
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+-- switch between tree and file view via Strg & Arrow
+vim.keymap.set("n", "<C-Right>", '<c-w>l', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Left>", '<c-w>h', { noremap = true, silent = true })
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
