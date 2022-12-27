814c49d8c6416ad2f6d10356c891755a796d630b
Dan <me.daniel.meier@gmail.com>
2022-12-27 14:55:50 +0100

    add gitui to toggleterm

--- init.lua
+++ init.lua
@@ -56,6 +56,16 @@ require'toggleterm'.setup {
   direction = 'float',
 }
 
+local Terminal  = require('toggleterm.terminal').Terminal
+local gitui = Terminal:new({ cmd = "gitui", hidden = true })
+
+function _gitui_toggle()
+  gitui:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _gitui_toggle()<CR>", {noremap = true, silent = true})
+
+-- lualine
 require('lualine').setup {
   options = {
     icons_enabled = true,
