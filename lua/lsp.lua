ad71e08d066fbf82ee241002be467c9c82c3d691
Dan <me.daniel.meier@gmail.com>
2024-02-06 10:18:53 +0100

    feat(fix): command mode suggestions
    Now, the mappings are wrapped with cmp.mapping() and restricted to insert and command-line modes ({ 'i', 'c' }). This change enhances the behavior of the mappings in specific modes.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,74 +1,74 @@
 -- terraform
-require'lspconfig'.terraformls.setup{}
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  pattern = {"*.tf", "*.tfvars"},
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+require 'lspconfig'.terraformls.setup {}
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = { "*.tf", "*.tfvars" },
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
 -- javascript
-require'lspconfig'.tsserver.setup {}
+require 'lspconfig'.tsserver.setup {}
 -- lua
 require("lspconfig").lua_ls.setup({})
 
 -- Set up nvim-cmp.
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-       require('luasnip').lsp_expand(args.body) -- For luasnip users.
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = {
-    ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
-    ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),     -- show completion suggestions
-    ['<C-e>'] = cmp.mapping.abort(),            -- close completion suggestions
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-    { name = 'buffer' },
-  },
-  completion = {
-    completeopt = 'menu,menuone,noinsert',
-  },
-  formatting = {
-    format = function(entry, vim_item)
-      -- Customize the appearance of each entry.
-      vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        buffer = "[Buffer]",
-        vsnip = "[VSnip]",
-        luasnip = "[LuaSnip]",
-        ultisnips = "[UltiSnips]",
-        snippy = "[Snippy]",
-      })[entry.source.name]
-      return vim_item
-    end,
-  },
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)  -- For luasnip users.
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = {
+		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }), -- previous suggestion
+		["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }), -- next suggestion
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
+		['<C-e>'] = cmp.mapping.abort(),        -- close completion suggestions
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' }, -- For luasnip users.
+		{ name = 'buffer' },
+	},
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+	},
+	formatting = {
+		format = function(entry, vim_item)
+			-- Customize the appearance of each entry.
+			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				buffer = "[Buffer]",
+				vsnip = "[VSnip]",
+				luasnip = "[LuaSnip]",
+				ultisnips = "[UltiSnips]",
+				snippy = "[Snippy]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.cmdline('/', {
-  sources = { { name = 'buffer' } },
+	sources = { { name = 'buffer' } },
 })
 
 cmp.setup.cmdline('?', {
-  sources = { { name = 'buffer' } },
+	sources = { { name = 'buffer' } },
 })
 
 cmp.setup.cmdline(':', {
-  sources = {
-    { name = 'path' },
-    { name = 'cmdline' },
-  },
+	sources = {
+		{ name = 'path' },
+		{ name = 'cmdline' },
+	},
 })
