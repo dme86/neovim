ddb5437bd4b3348269d4a3ba5b53fc40967964e7
Dan <me.daniel.meier@gmail.com>
2024-10-12 21:40:43 +0200

    feat(add): dme ascii
    Adding my 'dme' ascii to dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,8 +3,22 @@ return {
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
+      theme = 'hyper',
+      config = {
+
+	        header = {
+          '      _                ',
+          '     | |               ',
+          '   __| |_ __ ___   ___ ',
+          '  / _` | \'_ ` _ \\ / _ \\',
+          ' | (_| | | | | | |  __/',
+          '  \\__,_|_| |_| |_|\\___|',
+          '                       ',
+          '                       ',
+        },
       -- config
-    }
+    },
+  }
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
 }
