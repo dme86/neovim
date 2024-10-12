4f09f51135559859221dd2bf5e6c470cab201b85
Dan <me.daniel.meier@gmail.com>
2024-10-12 23:22:07 +0200

    chore(change): header
    Changing from default header.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -16,9 +16,34 @@ return {
           '                       ',
           '                       ',
         },
-      -- config
+      week_header = {
+       enable = false,
+      },
+      shortcut = {
+        { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+        {
+          icon = ' ',
+          icon_hl = '@variable',
+          desc = 'Files',
+          group = 'Label',
+          action = 'Telescope find_files',
+          key = 'f',
+        },
+        {
+          desc = ' Apps',
+          group = 'DiagnosticHint',
+          action = 'Telescope app',
+          key = 'a',
+        },
+        {
+          desc = ' dotfiles',
+          group = 'Number',
+          action = 'Telescope dotfiles',
+          key = 'd',
+        },      -- config
     },
   }
+}
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
 }
