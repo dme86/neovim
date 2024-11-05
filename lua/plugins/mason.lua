f0494a55edc6ada730b28fd90c88f530ccd843b6
Dan <me.daniel.meier@gmail.com>
2024-11-05 19:06:36 +0100

    chore(comment): helm
    don't want to use helmLS for now.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
       ensure_installed = { -- See: https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
 	"lua_ls",
 	"bashls",
-	"helm_ls",
+	-- "helm_ls",
 	"gopls",
 	"jsonls",
 	"marksman", -- markdown
@@ -24,7 +24,7 @@ return {
     -- Setup lspconfig for specific servers
     require('lspconfig').lua_ls.setup{}
     require('lspconfig').bashls.setup{}
-    require('lspconfig').helm_ls.setup{}
+    -- require('lspconfig').helm_ls.setup{}
     require('lspconfig').gopls.setup{}
     require('lspconfig').jsonls.setup{}
     require('lspconfig').marksman.setup{}
