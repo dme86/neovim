1df5a841d96c3f7ed0a9ae7c6a145dad57bba528
Dan <me.daniel.meier@gmail.com>
2024-11-07 11:34:59 +0100

    fix(add): helm lsp
    this commit prevents attachment and sorts out the yamlls errors on helm
    files.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,36 +1,37 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { -- See: https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-	"lua_ls",
-	"bashls",
-	-- "helm_ls",
-	"gopls",
-	"jsonls",
-	"marksman", -- markdown
-	"terraformls",
-	"dockerls",
-	"yamlls"
-      },
-      automatic_installation = true,
-    })
+    -- Mason LSP and dependencies
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls", "bashls", "helm_ls", "gopls", "jsonls", "marksman",
+                    "terraformls", "dockerls", "yamlls"
+                },
+                automatic_installation = true,
+            })
 
-    -- Setup lspconfig for specific servers
-    require('lspconfig').lua_ls.setup{}
-    require('lspconfig').bashls.setup{}
-    -- require('lspconfig').helm_ls.setup{}
-    require('lspconfig').gopls.setup{}
-    require('lspconfig').jsonls.setup{}
-    require('lspconfig').marksman.setup{}
-    require('lspconfig').terraformls.setup{}
-    require('lspconfig').dockerls.setup{}
-    require('lspconfig').yamlls.setup{}
-  end,
+            -- Setup lspconfig for specific servers
+            require('lspconfig').lua_ls.setup{}
+            require('lspconfig').bashls.setup{}
+            require('lspconfig').helm_ls.setup{}
+            require('lspconfig').gopls.setup{}
+            require('lspconfig').jsonls.setup{}
+            require('lspconfig').marksman.setup{}
+            require('lspconfig').terraformls.setup{}
+            require('lspconfig').dockerls.setup{}
+            require('lspconfig').yamlls.setup{}
+        end,
+    },
+
+    -- vim-helm plugin to prevent yamlls errors in Helm files
+    {
+        'towolf/vim-helm',
+        ft = 'helm'
+    },
 }
-
